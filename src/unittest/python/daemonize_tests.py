1f996d8ade1855844bf5ea71c6a93e3f056e2249
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2016-03-22 11:06:08 +0100

    Use setUp/tearDown to mock the sys/sys.argv

--- src/unittest/python/daemonize_tests.py
+++ src/unittest/python/daemonize_tests.py
@@ -13,6 +13,14 @@ from succubus import Daemon
 
 
 class TestDaemonize(TestCase):
+    def setUp(self):
+        self.mock_sys_context = patch("succubus.daemonize.sys")
+        self.mock_sys = self.mock_sys_context.__enter__()
+        self.mock_sys.argv = ['foo', 'bar']
+
+    def tearDown(self):
+        self.mock_sys_context.__exit__()
+
     @patch('succubus.daemonize.sys')
     def test_must_pop_sys_argv_before_loading_config(self, mock_sys):
         """The sys.argv.pop() must happen before load_configuration()
@@ -39,28 +47,24 @@ class TestDaemonize(TestCase):
 
         mock_setgid.assert_called_with(0)
 
-    @patch("succubus.daemonize.sys")
     @patch("succubus.daemonize.os.setgid")
-    def test_set_gid_exits_on_error(self, mock_setgid, mock_sys):
-        mock_sys.argv = ['foo', 'bar']
+    def test_set_gid_exits_on_error(self, mock_setgid):
         daemon = Daemon(pid_file="foo")
         daemon.group = "root"
         mock_setgid.side_effect = Exception
 
         daemon.set_gid()
 
-        mock_sys.exit.assert_called_once_with(1)
+        self.mock_sys.exit.assert_called_once_with(1)
 
-    @patch("succubus.daemonize.sys")
     @patch("succubus.daemonize.os.setgid")
-    def test_set_gid_does_nothing_by_default(self, mock_setgid, mock_sys):
-        mock_sys.argv = ['foo', 'bar']
+    def test_set_gid_does_nothing_by_default(self, mock_setgid):
         daemon = Daemon(pid_file="foo")
 
         daemon.set_gid()
 
         self.assertEqual(mock_setgid.call_count, 0)
-        self.assertEqual(mock_sys.exit.call_count, 0)
+        self.assertEqual(self.mock_sys.exit.call_count, 0)
 
     @patch("succubus.daemonize.os.setuid")
     def test_set_uid_translates_user_name(self, mock_setuid):
@@ -71,45 +75,34 @@ class TestDaemonize(TestCase):
 
         mock_setuid.assert_called_with(0)
 
-    @patch("succubus.daemonize.sys")
     @patch("succubus.daemonize.os.setuid")
-    def test_set_uid_exits_on_error(self, mock_setuid, mock_sys):
-        mock_sys.argv = ['foo', 'bar']
+    def test_set_uid_exits_on_error(self, mock_setuid):
         daemon = Daemon(pid_file="foo")
         daemon.user = "root"
         mock_setuid.side_effect = Exception
 
         daemon.set_uid()
 
-        mock_sys.exit.assert_called_once_with(1)
+        self.mock_sys.exit.assert_called_once_with(1)
 
-    @patch("succubus.daemonize.sys")
     @patch("succubus.daemonize.os.setuid")
-    def test_set_uid_does_nothing_by_default(self, mock_setuid, mock_sys):
-        mock_sys.argv = ['foo', 'bar']
+    def test_set_uid_does_nothing_by_default(self, mock_setuid):
         daemon = Daemon(pid_file="foo")
 
         daemon.set_uid()
 
         self.assertEqual(mock_setuid.call_count, 0)
-        self.assertEqual(mock_sys.exit.call_count, 0)
+        self.assertEqual(self.mock_sys.exit.call_count, 0)
 
-    @patch("succubus.daemonize.sys")
-    def test_daemon_insists_on_pidfile(self, mock_sys):
-        mock_sys.argv = ['foo', 'bar']
+    def test_daemon_insists_on_pidfile(self):
         self.assertRaisesRegexp(Exception, "You did not provide a pid file",
                                 Daemon)
 
-    @patch("succubus.daemonize.sys")
-    def test_daemon_remembers_abspath_of_pidfile(self, mock_sys):
-        mock_sys.argv = ['foo', 'bar']
+    def test_daemon_remembers_abspath_of_pidfile(self):
         daemon = Daemon(pid_file="foo")
         self.assertTrue(os.path.isabs(daemon.pid_file))
 
-    @patch("succubus.daemonize.sys")
-    def test_delpid_really_deletes_pidfile(self, mock_sys):
-        mock_sys.argv = ['foo', 'bar']
-
+    def test_delpid_really_deletes_pidfile(self):
         fake_pidfile = tempfile.NamedTemporaryFile()
         try:
             daemon = Daemon(pid_file=fake_pidfile.name)
@@ -119,10 +112,7 @@ class TestDaemonize(TestCase):
             if os.path.exists(fake_pidfile.name):
                 os.unlink(fake_pidfile.name)
 
-    @patch("succubus.daemonize.sys")
-    def test_already_running_detects_running_process(self, mock_sys):
-        mock_sys.argv = ['foo', 'bar']
-
+    def test_already_running_detects_running_process(self):
         fake_pidfile = tempfile.NamedTemporaryFile()
         try:
             content = "{pid}\n".format(pid=os.getpid())
@@ -136,17 +126,11 @@ class TestDaemonize(TestCase):
         finally:
             os.unlink(fake_pidfile.name)
 
-    @patch("succubus.daemonize.sys")
-    def test_already_running_handles_missing_pidfile(self, mock_sys):
-        mock_sys.argv = ['foo', 'bar']
-
+    def test_already_running_handles_missing_pidfile(self):
         daemon = Daemon(pid_file="/a_nonexisting_pidfile")
         self.assertEqual(daemon._already_running(), False)
 
-    @patch("succubus.daemonize.sys")
-    def test_already_running_handles_missing_process(self, mock_sys):
-        mock_sys.argv = ['foo', 'bar']
-
+    def test_already_running_handles_missing_process(self):
         # The test assumes that the OS will not reuse test_pid before
         # this function is finished.
         test_process = subprocess.Popen(['true'])
@@ -166,9 +150,7 @@ class TestDaemonize(TestCase):
 
     @patch("succubus.daemonize.time.sleep")
     @patch("succubus.daemonize.os.kill")
-    @patch("succubus.daemonize.sys")
-    def test_reliable_kill_actually_kills(self, mock_sys, mock_kill, mock_sleep):
-        mock_sys.argv = ['foo', 'bar']
+    def test_reliable_kill_actually_kills(self, mock_kill, mock_sleep):
         fake_pidfile = tempfile.NamedTemporaryFile()
         daemon = Daemon(pid_file=fake_pidfile.name)
         daemon.pid = 1234
@@ -192,10 +174,8 @@ class TestDaemonize(TestCase):
 
     @patch("succubus.daemonize.time.sleep")
     @patch("succubus.daemonize.os.kill")
-    @patch("succubus.daemonize.sys")
-    def test_reliable_kill_kills_stuck_processes(self, mock_sys, mock_kill, mock_sleep):
+    def test_reliable_kill_kills_stuck_processes(self, mock_kill, mock_sleep):
         """When a process ignores SIGTERM, it must be killed with SIGKILL"""
-        mock_sys.argv = ['foo', 'bar']
         daemon = Daemon(pid_file="foo")
         daemon.pid = 1234
 
@@ -210,14 +190,12 @@ class TestDaemonize(TestCase):
 
     @patch("succubus.daemonize.time.sleep")
     @patch("succubus.daemonize.os.kill")
-    @patch("succubus.daemonize.sys")
-    def test_reliable_kill_reportss_permission_problems(self, mock_sys, mock_kill, mock_sleep):
+    def test_reliable_kill_reportss_permission_problems(self, mock_kill, mock_sleep):
         """If the process cannot be killed, an error must be reported
 
         This typically happens when the user does not have sufficient
         permissions to kill the process.
         """
-        mock_sys.argv = ['foo', 'bar']
         daemon = Daemon(pid_file="foo")
         daemon.pid = 1234
 
@@ -227,9 +205,7 @@ class TestDaemonize(TestCase):
 
         self.assertEqual(retval, 1)
 
-    @patch("succubus.daemonize.sys")
-    def test_stop_terminates_a_running_process(self, mock_sys):
-        mock_sys.argv = ['foo', 'bar']
+    def test_stop_terminates_a_running_process(self):
         daemon = Daemon(pid_file="foo")
 
         daemon._already_running = Mock()
@@ -243,9 +219,7 @@ class TestDaemonize(TestCase):
         daemon.reliable_kill.assert_called_once_with()
         self.assertEqual(retval, 123)
 
-    @patch("succubus.daemonize.sys")
-    def test_stop_handles_missing_process(self, mock_sys):
-        mock_sys.argv = ['foo', 'bar']
+    def test_stop_handles_missing_process(self):
         daemon = Daemon(pid_file="foo")
 
         daemon._already_running = Mock()
@@ -258,9 +232,7 @@ class TestDaemonize(TestCase):
         self.assertEqual(daemon.reliable_kill.call_count, 0)
         self.assertEqual(retval, 0)
 
-    @patch("succubus.daemonize.sys")
-    def test_status_reports_running_process(self, mock_sys):
-        mock_sys.argv = ['foo', 'bar']
+    def test_status_reports_running_process(self):
         daemon = Daemon(pid_file="foo")
 
         daemon._already_running = Mock()
@@ -271,9 +243,7 @@ class TestDaemonize(TestCase):
         daemon._already_running.assert_called_once_with()
         self.assertEqual(retval, 0)
 
-    @patch("succubus.daemonize.sys")
-    def test_status_handles_missing_process(self, mock_sys):
-        mock_sys.argv = ['foo', 'bar']
+    def test_status_handles_missing_process(self):
         daemon = Daemon(pid_file="foo")
 
         daemon._already_running = Mock()
